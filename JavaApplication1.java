31ccec0a76f7756986e8011acfe2c82a78f87e0a
Rüveyda Dal <71987230+ruv19@users.noreply.github.com>
2021-04-26 16:55:06 +0300

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,36 @@
+
+package javaapplication1;
+import java.util.Scanner;
+
+ 
+public class JavaApplication1 {
+   
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String isim = "Ahmet",sifre= "Gedik";
+        
+        System.out.println("Kullanıcı ismi:");
+        String kul_ad=scan.nextLine();
+        System.out.println("Kullanıcı şifresi:");
+        String kul_sifre=scan.nextLine();
+        
+        
+        if(isim.equals(kul_ad) && !sifre.equals(kul_sifre) )
+        {
+            System.out.println("İsim doğru , şifre yanlış!");
+        }
+        else if(!isim.equals(kul_ad) && sifre.equals(kul_sifre))
+        {
+            System.out.print("İsim yanlış, şifre doğru!");
+        }
+        else if(isim.equals(kul_ad) && sifre.equals(kul_sifre))
+        {
+            System.out.println("Hoşgeldiniz!");
+        }
+        
+    }
+}
+
+    
---
+++ NewClass1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author muham
+ */
+public class NewClass1 {
+    public static void main (String [] args)
+    {
+        int x=0;
+        x=1+2*3;
+        System.out.print(x);
+    
+    }
+    
+    
+}
